db5e8172fd14687d2fe5ac52789250ade5f69d98
lizabugirrr <buhir.pn@ucu.edu.ua>
2025-04-09 03:52:57 +0300

    Update views.py

--- ruhevit/report_military/views.py
+++ ruhevit/report_military/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def report_military(request):
-    return render(request, 'report_military/index.html')
+    return render(request, 'report_military/report_military.html')
