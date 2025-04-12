6a1a1b34708fbcfbb53523bf65e7eeadc533aa0b
Marharyta <paduchak.pn@ucu.edu.ua>
2025-04-12 12:02:38 +0300

    change in core/views.py for authorized users

--- ruhevit/core/views.py
+++ ruhevit/core/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from requests.models import Request
 
 def home_redirect(request):
     if request.user.is_authenticated:
-        return render(request, 'home/index.html')
+        user = request.user
+        user_requests = Request.objects.filter(owner=user).order_by('-created_at')
+        help_offers = Request.objects.filter(status='need_volunteer').exclude(owner=user).order_by('-created_at')
+        context = {
+            'user_requests': user_requests,
+            'help_offers': help_offers,
+        }
+        return render(request, 'home/index.html', context)
     else:
         return render(request, 'landing/index.html')
-
 
 def custom_404_view(request, exception):
     return render(request, 'errors/404.html', status=404)
--- ruhevit/home/views.py
+++ ruhevit/home/views.py
@@ -7,7 +7,8 @@ def index(request):
     user_requests = Request.objects.filter(owner=request.user).order_by('-created_at')
 
     help_offers = Request.objects.filter(status='need_volunteer').exclude(owner=request.user).order_by('-created_at')
-
+    print("User requests count:", user_requests.count())
+    print("Help offers count:", help_offers.count())
     context = {
         'user_requests': user_requests,
         'help_offers': help_offers,
