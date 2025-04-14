1dc1a153168f1f149e11c32861a8341b57f91d8a
Maryna Ohinska <maryna.ohinska@gmail.com>
2025-04-14 23:15:57 +0300

    Fixed reset pass from editing acc page

--- ruhevit/changing_info/urls.py
+++ ruhevit/changing_info/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.changing_info, name='changing_info'),
+    path('accounts/password_reset/', RedirectView.as_view(url='/accounts/password_reset/')),
 ]
