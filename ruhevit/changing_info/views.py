eb4bf3f4fb24d53e305459a50a71213d9d71b184
Marharyta <paduchak.pn@ucu.edu.ua>
2025-04-14 17:35:56 +0300

    Редагування профілю

--- ruhevit/changing_info/views.py
+++ ruhevit/changing_info/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
 
+@login_required
 def changing_info(request):
-    return render(request, 'changing_info/index.html')
+    user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            # Після збереження можна перенаправити на сторінку профілю або показати повідомлення
+            return redirect('profile_page')
+    else:
+        form = ProfileForm(instance=user)
+    return render(request, 'changing_info/index.html', {'form': form})
