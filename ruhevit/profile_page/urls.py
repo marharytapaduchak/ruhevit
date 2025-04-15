501be7e503e8fc2f059a5ecdcdf875d5ea926520
Maryna Ohinska <maryna.ohinska@gmail.com>
2025-04-15 12:46:30 +0300

    Button fix on landing

--- ruhevit/profile_page/urls.py
+++ ruhevit/profile_page/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.profile_page, name='profile_page'),
     path('logout/', LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
     path('requests/', include('requests.urls')),
+    
 ]
