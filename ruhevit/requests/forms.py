3733898d7e7ebc319a47fd3be791b82f125c266b
Alina Bodnar <bodnar.pn@ucu.edu.ua>
2025-04-24 09:35:29 +0300

    fixed form style

--- ruhevit/requests/forms.py
+++ ruhevit/requests/forms.py
@@ -1,20 +1,50 @@
 from django import forms
 from .models import *
 
+# class RequestHistoryForm(forms.ModelForm):
+#     class Meta:
+#         model = RequestHistory
+#         fields = ['status', 'comment']
+#         widgets = {
+#             'comment': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Опишіть, що вже зроблено...'}),
+#         }
+
+
+# class RequestForm(forms.ModelForm):
+#     class Meta:
+#         model = Request
+#         fields = ['name', 'description',
+#                 'priority', 'location', 'type']
+#         widgets = {
+#             'description': forms.Textarea(attrs={'rows': 4}),
+#         }
+
+
 class RequestHistoryForm(forms.ModelForm):
     class Meta:
         model = RequestHistory
         fields = ['status', 'comment']
         widgets = {
-            'comment': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Опишіть, що вже зроблено...'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'comment': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Опишіть, що вже зроблено...'
+            }),
         }
 
 
 class RequestForm(forms.ModelForm):
     class Meta:
         model = Request
-        fields = ['name', 'description',
-                'priority', 'location', 'type']
+        fields = ['name', 'description', 'priority', 'location', 'type']
         widgets = {
-            'description': forms.Textarea(attrs={'rows': 4}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4
+            }),
+            'priority': forms.Select(attrs={'class': 'form-control'}),
+            'location': forms.Select(attrs={'class': 'form-control'}),
+            'type': forms.Select(attrs={'class': 'form-control'}),
         }
